7f313eef603abe57e9fc5ceca07d989070b978c8
Ross McIlwaine <rdm0423@gmail.com>
2015-03-09 14:38:50 -0600

    Latest Version

--- OptionsToolbar/OTViewController.m
+++ OptionsToolbar/OTViewController.m
@@ -10,6 +10,9 @@
 
 @interface OTViewController ()
 
+@property (weak, nonatomic) IBOutlet UIToolbar *toolBar;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
 @end
 
 @implementation OTViewController
@@ -19,6 +22,42 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
+
+- (IBAction)firstJoke:(id)sender {
+    
+    self.label.text = @"A bear walks into a bar and says to the bartender, ‘I’ll have a whisky and ……… soda.’ The bartender says, ‘Why the big pause?’ ‘Dunno,’ says the bear. ‘I’ve always had them.’";
+    
+    self.view.backgroundColor = [UIColor lightGrayColor];
+    
+}
+
+- (IBAction)secondJoke:(id)sender {
+    
+    self.label.text = @"A group of fonts walk into a bar. ‘Get out of my pub!’ shouts the barman. ‘We don’t serve your type in here.’";
+    
+    self.view.backgroundColor = [UIColor redColor];
+    
+}
+
+- (IBAction)thirdJoke:(id)sender {
+    
+    self.label.text = @"A horse walks into a bar and orders a drink. The bartender says, ‘Why the long face?’";
+    
+    self.view.backgroundColor = [UIColor cyanColor];
+    
+}
+
+- (IBAction)fourthJoke:(id)sender {
+    
+    self.label.text = @"A pirate walks into a bar with a ship's steering-wheel attached to the front of his pants. The bartender says, 'You know you got a ship's steering wheel stuck to the front of your pants? The pirate says, Arrr, it's drivin' me nuts.";
+    
+    self.view.backgroundColor = [UIColor yellowColor];
+    
+}
+
+
+
+
 
 - (void)didReceiveMemoryWarning
 {
